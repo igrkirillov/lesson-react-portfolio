df4ce48130f3211997f4bab8d339cd4d4a8fa02c
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-13 11:58:28 +0300

    commit

--- src/components/portfolio-list/PortfolioList.jsx
+++ src/components/portfolio-list/PortfolioList.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import styles from "../portfolio-list/portfolio-list.module.css";
 
 export function PortfolioList({projects}) {
-  let index = 0;
   return (
   <div className={styles.container}>
       {projects.map(project => (
         <img
-          key={index++}
+          key={project.img}
           className={styles.element}
           src={project.img}
           alt={project.category}/>
